7af47759aa09c110cf7a2ce882e6316672c3db03
Magnus Hübner <magnus.hubner@aptitud.se>
2023-07-05 21:59:22 +0200

    fix: Mapped component tokens for Input to semantic tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -320,7 +320,7 @@ export const input = {
   default: 'block text-m mb-0 leading-m i-text-$color-input-text-filled i-bg-$color-input-background i-border-$color-input-border hover:i-border-$color-input-border-hover active:i-border-$color-input-border-active rounded-4 py-12 px-8 block border-1 w-full focusable focus:[--w-outline-offset:-2px] caret-current',
   textArea: 'min-h-[42] sm:min-h-[45]',
   disabled: 'i-bg-$color-input-background-disabled i-border-$color-input-border-disabled hover:i-border-$color-input-border-disabled! i-text-$color-input-text-disabled pointer-events-none',
-  invalid: 'i-border-$color-input-border-error i-text-$color-input-text-error!',
+  invalid: 'i-border-$color-input-border-negative i-text-$color-input-text-negative!',
   readOnly: 'pl-0 bg-transparent border-0 pointer-events-none i-text-$color-input-text-read-only',
   placeholder: 'placeholder:i-text-$color-input-text-placeholder',
   wrapper: 'relative',
