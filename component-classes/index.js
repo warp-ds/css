c8f932f0bf877f5007e07f8a2867d47b3ca0b3dd
Magnus Hübner <magnuh@users.noreply.github.com>
2023-07-05 08:59:51 +0200

    fix: Add tokens for the close icon in the Toast component (#26)

--- component-classes/index.js
+++ component-classes/index.js
@@ -166,7 +166,7 @@ export const toast = {
   iconNegative: 'i-text-$color-toast-negative-icon',
   iconLoading: 'animate-bounce',
   content: 'self-center mr-8 py-4 last-child:mb-0',
-  close: 'bg-transparent ml-auto p-[8px]',
+  close: 'bg-transparent ml-auto p-[8px] i-text-$color-toast-close-icon hover:i-text-$color-toast-close-icon-hover active:i-text-$color-toast-close-icon-active',
 };
 
 export const tabs = {
