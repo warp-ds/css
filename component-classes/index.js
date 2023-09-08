92af46af5b0a233f611fe7183df72abf13d84b64
Magnus Hübner <magnuh@users.noreply.github.com>
2023-09-08 13:48:48 +0200

    feat: Add component-classes for Badge (#48)

--- component-classes/index.js
+++ component-classes/index.js
@@ -25,6 +25,7 @@ export const pageIndicator = {
   active: 'i-bg-$color-pageindicator-background-selected',
 };
 
+// Deprecated: Use Badge component
 export const ribbon = {
   base: 'py-4 px-8 border rounded-4 inline-flex last:mb-0',
   info: 'i-border-$color-badge-info-background i-bg-$color-badge-info-background i-text-$color-badge-info-text',
@@ -36,6 +37,23 @@ export const ribbon = {
   neutral: 'i-border-$color-badge-neutral-background i-bg-$color-badge-neutral-background i-text-$color-badge-neutral-text',
   roundedTopRightBottomLeft: 'rounded-tr-0 rounded-bl-0',
   roundedTopLeftBottomRight: 'rounded-tl-0 rounded-br-0',
+};
+
+export const badge = {
+  base: 'py-4 px-8 border-0 rounded-4 text-xs inline-flex',
+  neutral: 'i-bg-$color-badge-neutral-background i-text-$color-badge-neutral-text',
+  info: 'i-bg-$color-badge-info-background i-text-$color-badge-info-text',
+  positive: 'i-bg-$color-badge-positive-background i-text-$color-badge-positive-text',
+  warning: 'i-bg-$color-badge-warning-background i-text-$color-badge-warning-text',
+  negative: 'i-bg-$color-badge-negative-background i-text-$color-badge-negative-text',
+  disabled: 'i-bg-$color-badge-disabled-background i-text-$color-badge-disabled-text',
+  price: 'i-bg-$color-badge-price-background i-text-$color-badge-price-text',
+  notification: 'i-bg-$color-badge-notification-background i-text-$color-badge-notification-text',
+  positionBase: 'absolute backdrop-blur',
+  positionTL: 'rounded-tl-0 rounded-tr-0 rounded-bl-0 top-0 left-0',
+  positionTR: 'rounded-tl-0 rounded-tr-0 rounded-br-0 top-0 right-0',
+  positionBR: 'rounded-tr-0 rounded-br-0 rounded-bl-0 bottom-0 right-0',
+  positionBL: 'rounded-tl-0 rounded-br-0 rounded-bl-0 bottom-0 left-0',
 };
 
 export const slider = {
