c1fa42dbb8911b2a85f5038ecfd3d7962e8a2cc2
Anna Rybkina <anna.rybkina@anicode.se>
2023-10-06 08:52:16 +0200

    fix: remove duplicates from the classes list

--- component-classes/classes.js
+++ component-classes/classes.js
@@ -1,8 +1,12 @@
 import * as components from './index.js';
 
-export const classes = Object.values(components)
-  .map((e) => {
-    if (typeof e === 'object') return Object.values(e).map((e) => e.split(/\s/));
-    return e.split(/\s/);
-  })
-  .flat(Infinity);
+const getClasses = () =>
+  Object.values(components)
+    .map((e) => {
+      if (typeof e === 'object')
+        return Object.values(e).map((e) => e.split(/\s/));
+      return e.split(/\s/);
+    })
+    .flat(Infinity);
+
+export const classes = [...new Set(getClasses())];
