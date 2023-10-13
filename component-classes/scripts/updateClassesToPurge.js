f1754a7431632c26ad282481335e7ea010f85091
Anna Rybkina <anna.rybkina@anicode.se>
2023-10-13 08:26:14 +0200

    chore: put classesToPurge to dist for publishing to Eik

--- component-classes/scripts/updateClassesToPurge.js
+++ component-classes/scripts/updateClassesToPurge.js
@@ -3,4 +3,9 @@ import { classes } from '../classes.js';
 
 export default () => {
   fs.writeFileSync('./classesToPurge.json', JSON.stringify(classes), 'utf-8');
+  fs.writeFileSync(
+    '../dist/classesToPurge.json',
+    JSON.stringify(classes),
+    'utf-8'
+  );
 };
