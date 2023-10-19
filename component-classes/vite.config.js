22504678f5a44afd9a1262e2e7f855ba9851c46e
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-10-19 13:27:01 +0200

    feat: add classes to purge as json to a separate file and rename components.css to warp.css (#56)
    * fix: add classes to purge as json to a separate file
    * feat: rename components.css to warp.css
    
    BREAKING CHANGE: we rename components.css to warp.css as it can be used without using any components

--- component-classes/vite.config.js
+++ component-classes/vite.config.js
@@ -21,7 +21,7 @@ export default () => ({
     rollupOptions: {
       output: {
         dir: '../dist',
-        assetFileNames: `components.[ext]`,
+        assetFileNames: `warp.[ext]`,
       },
     },
   },
