ebe8dbaa6643382da6125f267094122d5b8a4c92
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-24 11:28:47 +0200

    chore: Remove redundant breadcrumbs tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -461,9 +461,9 @@ export const prefix = {
 
 export const breadcrumbs = {
   wrapper: 'flex space-x-8',
-  text: 'i-text-$color-breadcrumbs-text',
-  link: 'i-text-$color-breadcrumbs-link-text',
-  separator: 'select-none i-text-$color-breadcrumbs-icon',
+  text: 's-text',
+  link: 's-text-link',
+  separator: 'select-none s-text',
   a11y: 'sr-only',
 };
 
