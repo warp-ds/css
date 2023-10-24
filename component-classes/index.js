077ecd380a10270a2a2f7e8cffe14985c0553ea1
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-24 15:00:51 +0200

    chore: Remove redundant card tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -112,18 +112,17 @@ export const steps = {
 
 export const card = {
   card: 'cursor-pointer overflow-hidden relative transition-all',
-  cardShadow: 'rounded-8 i-shadow-$shadow-card hover:i-shadow-$shadow-card-hover hover:i-bg-$color-card-background-hover tap-highlight-transparent',
+  cardShadow: 'rounded-8 shadow-s hover:shadow-m hover:s-bg-hover tap-highlight-transparent',
   cardFlat: 'border-2 rounded-4',
   cardFlatUnselected:
-    'i-bg-$color-card-flat-background i-border-$color-card-flat-border hover:i-bg-$color-card-flat-background-hover hover:i-border-$color-card-flat-border-hover active:i-bg-$color-card-flat-background-active active:i-border-$color-card-flat-border-active',
+    's-bg s-border hover:s-bg-subtle-hover hover:s-border-hover active:s-bg-active active:s-border-active',
   cardFlatSelected:
-    'i-border-$color-card-flat-border-selected i-bg-$color-card-flat-background-selected hover:i-bg-$color-card-flat-background-selected-hover hover:i-border-$color-card-flat-border-selected-hover active:i-border-$color-card-flat-border-active active:i-bg-$color-card-flat-background-active',
+    's-border-selected s-bg-selected hover:s-bg-selected-hover hover:s-border-selected-hover active:s-border-selected-active active:s-bg-selected-active',
   cardSelected:
-    'i-border-$color-card-border-selected i-bg-$color-card-background-selected hover:i-border-$color-card-border-selected-hover hover:i-bg-$color-card-background-selected-hover active:i-border-$color-card-border-selected-active',
-  cardOutline:
-    'active:i-border-$color-card-flat-border absolute rounded-8 inset-0 transition-all border-2',
-  cardOutlineUnselected: 'i-border-$color-card-border',
-  cardOutlineSelected: 'i-border-$color-card-border-selected hover:i-border-$color-card-border-selected-hover',
+    's-border-selected s-bg-selected hover:s-border-selected-hover hover:s-bg-selected-hover active:s-border-selected-active',
+  cardOutline: 'active:s-border absolute rounded-8 inset-0 transition-all border-2',
+  cardOutlineUnselected: 'border-transparent',
+  cardOutlineSelected: 's-border-selected hover:s-border-selected-hover',
   a11y: 'sr-only',
 };
 
