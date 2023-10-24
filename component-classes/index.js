d5394be3bbfa65ef073f256406b1813e6498b218
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-24 12:49:31 +0200

    chore: Remove redundant toast tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -152,18 +152,17 @@ export const toaster = {
 
 export const toast = {
   wrapper: 'relative overflow-hidden w-full',
-  toast:
-    'flex group p-8 mt-16 rounded-8 border-2 w-full pointer-events-auto transition-all',
-  positive: 'i-bg-$color-toast-positive-background i-border-$color-toast-positive-subtle-border i-text-$color-toast-positive-text',
-  warning: 'i-bg-$color-toast-warning-background i-border-$color-toast-warning-subtle-border i-text-$color-toast-warning-text',
-  negative: 'i-bg-$color-toast-negative-background i-border-$color-toast-negative-subtle-border i-text-$color-toast-negative-text',
+  toast: 'flex group p-8 mt-16 rounded-8 border-2 w-full pointer-events-auto transition-all s-text',
+  positive: 's-bg-positive-subtle s-border-positive',
+  warning: 's-bg-warning-subtle s-border-warning-subtle',
+  negative: 's-bg-negative-subtle s-border-negative-subtle',
   icon: 'shrink-0 rounded-full w-[16px] h-[16px] m-[8px]',
-  iconPositive: 'i-text-$color-toast-positive-icon',
-  iconWarning: 'i-text-$color-toast-warning-icon',
-  iconNegative: 'i-text-$color-toast-negative-icon',
+  iconPositive: 's-icon-positive',
+  iconWarning: 's-icon-warning',
+  iconNegative: 's-icon-negative',
   iconLoading: 'animate-bounce',
   content: 'self-center mr-8 py-4 last-child:mb-0',
-  close: 'bg-transparent ml-auto p-[8px] i-text-$color-toast-close-icon hover:i-text-$color-toast-close-icon-hover active:i-text-$color-toast-close-icon-active',
+  close: 'bg-transparent ml-auto p-[8px] s-icon hover:s-icon-hover active:s-icon-active',
 };
 
 export const tabs = {
