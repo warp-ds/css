053bb26b9dfdc1d882a2bc48602027f454e691b0
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-31 11:23:54 +0100

    chore: Remove redundant select tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -416,13 +416,13 @@ export const input = {
 };
 
 export const select = {
-  default: 'block text-m mb-0 leading-m i-text-$color-select-text i-bg-$color-select-background i-border-$color-select-border hover:i-border-$color-select-border-hover active:i-border-$color-select-border-active rounded-4 py-12 px-8 block border-1 w-full focusable focus:[--w-outline-offset:-2px] appearance-none pr-32 cursor-pointer caret-current',
-  disabled: 'i-bg-$color-select-background-disabled i-border-$color-select-border-disabled hover:i-border-$color-select-border-disabled! active:i-border-$color-select-border-disabled! i-text-$color-select-text-disabled pointer-events-none',
-  invalid: 'i-border-$color-select-border-negative',
+  default: 'block text-m mb-0 leading-m s-text-text s-bg s-border hover:s-border-hover active:s-border-active rounded-4 py-12 px-8 block border-1 w-full focusable focus:[--w-outline-offset:-2px] appearance-none pr-32 cursor-pointer caret-current',
+  disabled: 'i-bg-$color-select-background-disabled s-border-disabled hover:s-border-disabled! active:s-border-disabled! s-text-disabled pointer-events-none',
+  invalid: 's-border-negative',
   readOnly: 'pl-0 bg-transparent border-0 pointer-events-none before:hidden',
   wrapper: 'relative',
   selectWrapper: `relative before:block before:absolute before:right-0 before:bottom-0 before:w-32 before:h-full before:pointer-events-none `,
-  chevron: 'absolute top-[30%] block right-0 bottom-0 w-32 h-full i-text-$color-select-icon pointer-events-none cursor-pointer',
+  chevron: 'absolute top-[30%] block right-0 bottom-0 w-32 h-full s-icon pointer-events-none cursor-pointer',
   chevronDisabled: 'opacity-25',
 };
 
