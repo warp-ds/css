fe39c2327d63e7bbc2817b42e69575369cbf789b
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-31 10:31:34 +0100

    chore: Remove redundant pageindicator tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -1,8 +1,8 @@
 export const pageIndicator = {
   wrapper: 'flex space-x-8 p-8',
   dot: 'h-8 w-8 rounded-full',
-  inactive: 'i-bg-$color-pageindicator-background hover:i-bg-$color-pageindicator-background-hover',
-  active: 'i-bg-$color-pageindicator-background-selected',
+  inactive: 's-bg-subtle hover:s-bg-subtle-hover',
+  active: 's-bg-primary',
 };
 
 // Deprecated: Use Badge component
