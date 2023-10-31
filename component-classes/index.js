a867db864718845987e522cc3bda88aa46d7553c
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-31 13:56:49 +0100

    chore: Remove redundant stepindicator tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -79,11 +79,11 @@ export const step = {
   stepVerticalRight: 'grid-cols-[1fr_20px] text-right',
   stepHorizontal: 'group/steph grid-rows-[auto_20px] grid-cols-[1fr_20px_1fr] flex-1 grid gap-y-16 items-center',
 
-  stepDot: 'rounded-full border-2 h-20 w-20 transition-colors duration-300 i-text-$color-stepindicator-handle-icon',
+  stepDot: 'rounded-full border-2 h-20 w-20 transition-colors duration-300 s-icon-inverted',
   stepDotVerticalRight: 'col-start-2',
   stepDotHorizontal: 'row-start-2 justify-self-end',
-  stepDotActive: 'i-border-$color-stepindicator-handle-border-active i-bg-$color-stepindicator-handle-background-active',
-  stepDotIncomplete: 'i-border-$color-stepindicator-handle-border i-bg-$color-stepindicator-handle-background',
+  stepDotActive: 's-border-selected s-bg-primary',
+  stepDotIncomplete: 's-border s-bg',
 
   stepLine: 'group-last/stepv:hidden transition-colors duration-300',
   stepLineVertical: 'w-2 h-full justify-self-center',
@@ -92,8 +92,8 @@ export const step = {
   stepLineHorizontalRight: 'group-last/steph:bg-transparent',
   stepLineHorizontalLeft: 'group-first/steph:bg-transparent',
 
-  stepLineIncomplete: 'i-bg-$color-stepindicator-track-background',
-  stepLineComplete: 'i-bg-$color-stepindicator-track-background-active',
+  stepLineIncomplete: 's-bg-disabled',
+  stepLineComplete: 's-bg-primary',
 
   content: 'last:mb-0 group-last/step:last:pb-0',
   contentVertical: 'row-span-2 pb-32',
