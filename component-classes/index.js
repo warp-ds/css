1680c7949ddae50df2cead15df201eed99905ae1
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-31 16:28:22 +0100

    chore: Remove redundant tabs tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -159,20 +159,18 @@ export const toast = {
 
 export const tabs = {
   tabContainer: 'mx-auto max-w-screen-md w-full grid relative',
-  wunderbar:
-    'absolute i-border-$color-tabs-border-selected -bottom-0 border-b-4 transition-all',
-  wrapperUnderlined:
-    'border-b i-border-$color-tabs-border -mx-16 sm:mx-0 px-4 sm:px-0 mb-32 ',
+  wunderbar: 'absolute s-border-selected -bottom-0 border-b-4 transition-all',
+  wrapperUnderlined: 'border-b border-transparent -mx-16 sm:mx-0 px-4 sm:px-0 mb-32 ',
 };
 
 export const tab = {
-  tab: 'grid items-center font-bold gap-8 focusable antialias p-16 pb-8 border-b-4 bg-transparent i-text-$color-tabs-text i-border-$color-tabs-border hover:i-text-$color-tabs-text-hover hover:i-border-$color-tabs-border-hover',
-  tabActive: 'i-text-$color-tabs-text-selected',
-  icon: 'mx-auto hover:i-text-$color-tabs-text-hover',
-  iconUnderlinedActive: 'i-text-$color-tabs-text-selected',
+  tab: 'grid items-center font-bold gap-8 focusable antialias p-16 pb-8 border-b-4 bg-transparent s-text-subtle border-transparent hover:s-text-link hover:s-border-selected',
+  tabActive: 's-text-link',
+  icon: 'mx-auto hover:s-text-link',
+  iconUnderlinedActive: 's-text-link',
   content: 'flex items-center justify-center gap-8',
   contentUnderlined: 'content-underlined', // content-underlined is a no-op that prevents a quirk in how Vue handles class bindings
-  contentUnderlinedActive: 'i-text-$color-tabs-text-selected',
+  contentUnderlinedActive: 's-text-link',
 };
 
 // Todo: Handle dynamic classnames
