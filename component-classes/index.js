4d499dfc13ef69ac0881bac10fe94474c2fbd879
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-31 10:04:46 +0100

    chore: Remove redundant helptext tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -433,9 +433,9 @@ export const label = {
 };
 
 export const helpText = {
-  helpText: 'text-xs mt-4 block i-text-$color-helptext-text',
-  helpTextValid: 'i-text-$color-helptext-text-positive',
-  helpTextInvalid: 'i-text-$color-helptext-text-negative',
+  helpText: 'text-xs mt-4 block s-text-subtle',
+  helpTextValid: 's-text-positive',
+  helpTextInvalid: 's-text-negative',
 };
 
 const prefixSuffixWrapperBase =
