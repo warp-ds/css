53c02cc2e48a254e4bafdf220505adc0ab6d3b45
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-31 10:15:18 +0100

    chore: Remove redundant input tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -404,12 +404,12 @@ export const alert = {
 };
 
 export const input = {
-  default: 'block text-m mb-0 leading-m i-text-$color-input-text-filled i-bg-$color-input-background i-border-$color-input-border hover:i-border-$color-input-border-hover active:i-border-$color-input-border-active rounded-4 py-12 px-8 block border-1 w-full focusable focus:[--w-outline-offset:-2px] caret-current',
+  default: 'block text-m mb-0 leading-m s-text s-bg s-border hover:s-border-hover active:s-border-selected rounded-4 py-12 px-8 block border-1 w-full focusable focus:[--w-outline-offset:-2px] caret-current',
   textArea: 'min-h-[42] sm:min-h-[45]',
-  disabled: 'i-bg-$color-input-background-disabled i-border-$color-input-border-disabled hover:i-border-$color-input-border-disabled! i-text-$color-input-text-disabled pointer-events-none',
-  invalid: 'i-border-$color-input-border-negative i-text-$color-input-text-negative!',
-  readOnly: 'pl-0 bg-transparent border-0 pointer-events-none i-text-$color-input-text-read-only',
-  placeholder: 'placeholder:i-text-$color-input-text-placeholder',
+  disabled: 'i-bg-$color-input-background-disabled s-border-disabled hover:s-border-disabled! s-text-disabled pointer-events-none',
+  invalid: 's-border-negative s-text-negative!',
+  readOnly: 'pl-0 bg-transparent border-0 pointer-events-none s-text',
+  placeholder: 'placeholder:s-text-placeholder',
   wrapper: 'relative',
   suffix: 'pr-40',
   prefix: 'pl-40',
