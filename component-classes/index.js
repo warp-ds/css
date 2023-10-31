ea96a513f854804deb572837ccb5aa6c5fdf1bce
Magnus Hübner <magnus.hubner@aptitud.se>
2023-10-31 09:58:55 +0100

    chore: Remove redundant combobox tokens

--- component-classes/index.js
+++ component-classes/index.js
@@ -506,11 +506,11 @@ export const clickable = {
 
 export const combobox = {
   wrapper: 'relative',
-  combobox: 'absolute left-0 right-0 pb-8 rounded-8 i-bg-$color-combobox-background i-shadow-$shadow-combobox',
+  combobox: 'absolute left-0 right-0 pb-8 rounded-8 s-bg shadow-m',
   textMatch: 'font-bold',
   listbox: 'm-0 p-0 select-none list-none',
-  option: 'block cursor-pointer p-8 hover:i-bg-$color-combobox-option-background-hover',
-  optionSelected: 'i-bg-$color-combobox-option-background-selected hover:i-bg-$color-combobox-option-background-selected-hover',
+  option: 'block cursor-pointer p-8 hover:s-bg-hover',
+  optionSelected: 's-bg-selected hover:s-bg-selected-hover',
   a11y: 'sr-only',
 };
 
