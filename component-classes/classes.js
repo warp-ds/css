657abc39dc8d2d6fdcfcbb915fec3526e7a46f1c
Magnus Hübner <magnus.hubner@aptitud.se>
2023-11-02 16:56:03 +0100

    Revert "feat: add classes to purge as json to a separate file and rename components.css to warp.css (#56)"
    This reverts commit 741750a461e2a31a4b42a52a614d010f00c58f16.

--- component-classes/classes.js
+++ component-classes/classes.js
@@ -1,12 +1,8 @@
 import * as components from './index.js';
 
-const getClasses = () =>
-  Object.values(components)
-    .map((e) => {
-      if (typeof e === 'object')
-        return Object.values(e).map((e) => e.split(/\s/));
-      return e.split(/\s/);
-    })
-    .flat(Infinity);
-
-export const classes = [...new Set(getClasses())];
+export const classes = Object.values(components)
+  .map((e) => {
+    if (typeof e === 'object') return Object.values(e).map((e) => e.split(/\s/));
+    return e.split(/\s/);
+  })
+  .flat(Infinity);
