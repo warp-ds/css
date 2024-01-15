8502e292e17c47368e1bd6efd3be3dda5614026f
BalbinaK <balbuhhha@gmail.com>
2024-01-15 17:57:21 +0100

    Fix linting with @warp-ds/esling-config v1.0.4

--- css-minify.js
+++ css-minify.js
@@ -1,11 +1,11 @@
 import * as lightning from 'lightningcss';
 
-export const minify = css => {
+export const minify = (css) => {
   const { code } = lightning.transform({
     code: Buffer.from(css),
     minify: true,
     targets: {
-      safari: (13 << 16),
+      safari: 13 << 16,
     },
   });
 
