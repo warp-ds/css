5ae5da9c0f53b5710788c83e4311c127800813af
imprashast <imprashast@gmail.com>
2024-06-05 12:34:57 +0200

    Remove redundant tokens and generate prod ready css tokens

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
+import path from 'node:path';
+
+import tokenize from '@warp-ds/tokenizer';
 import fs from 'fs-extra';
 import { transform } from 'lightningcss';
-import unzipper from 'unzipper';
 
-import { downloadFile } from './utils.js';
+import { init, downloadReleaseFile, brandToName } from './utils.js';
 
-fs.removeSync('tmp/web.zip');
-fs.removeSync('output/web');
-fs.ensureDirSync('tmp');
-fs.ensureDirSync('output/web');
+init();
 
-await downloadFile('https://github.com/warp-ds/tokens/releases/download/latest/web.zip', 'web.zip');
+await downloadReleaseFile();
 
-await unzipper.Open.file('tmp/web.zip').then((d) => d.extract({ path: 'output/web', concurrency: 5 }));
+const styleDictionaryTokensDir = 'output/web';
+const brandModes = fs
+  .readdirSync(styleDictionaryTokensDir)
+  .filter((item) => fs.statSync(path.join(styleDictionaryTokensDir, item)).isDirectory());
 
-let cssHex = fs.readFileSync('./output/web/FINN light/variables.css', 'utf8');
-let cssRgb = fs.readFileSync('./output/web/FINN light/variables-rgb.css', 'utf8');
-// this converts rgb(255, 255, 255) to 255, 255, 255 which is needed for unocss to work
-let regex = /rgb\(([^)]+)\)/g;
+brandModes.forEach((brandMode) => {
+  // skip dark themes for now
+  if (brandMode.includes('dark')) return;
 
-// these are in hex
-cssHex = cssHex.replaceAll(':root ', ':root,:host ');
-cssHex = cssHex.replaceAll('--color-', '--w-');
-cssHex = cssHex.replaceAll('--semantic-color-', '--w-s-color-');
-cssHex = cssHex.replaceAll('--components-', '--w-color-');
-cssHex = cssHex.replaceAll('-default:', ':');
+  console.log(`Processing ${brandMode}...`);
+  let cssHex = fs.readFileSync(`./${styleDictionaryTokensDir}/${brandMode}/variables.css`, 'utf8');
+  let cssRgb = fs.readFileSync(`./${styleDictionaryTokensDir}/${brandMode}/variables-rgb.css`, 'utf8');
+  // this converts rgb(255, 255, 255) to 255, 255, 255 which is needed for unocss to work
+  let regex = /rgb\(([^)]+)\)/g;
 
-// these are in rgb
-cssRgb = cssRgb.replaceAll(':root ', ':root,:host ');
-cssRgb = cssRgb.replaceAll('--color-', '--w-rgb-');
-cssRgb = cssRgb.replaceAll('--semantic-color-', '--w-s-rgb-');
-cssRgb = cssRgb.replaceAll(/--components-.*?:\s*var\(\s*--.*\);/g, '');
-cssRgb = cssRgb.replaceAll('-default:', ':');
+  // these are in hex
+  cssHex = cssHex.replaceAll(':root ', ':root,:host ');
+  cssHex = cssHex.replaceAll('--color-', '--w-');
+  cssHex = cssHex.replaceAll('--semantic-color-', '--w-s-color-');
+  cssHex = cssHex.replaceAll('--components-', '--w-color-');
+  cssHex = cssHex.replaceAll('-default:', ':');
 
-cssRgb = cssRgb.replaceAll(regex, '$1');
+  // these are in rgb
+  cssRgb = cssRgb.replaceAll(':root ', ':root,:host ');
+  cssRgb = cssRgb.replaceAll('--color-', '--w-rgb-');
+  cssRgb = cssRgb.replaceAll('--semantic-color-', '--w-s-rgb-');
+  cssRgb = cssRgb.replaceAll(/--components-.*?:\s*var\(\s*--.*\);/g, '');
+  cssRgb = cssRgb.replaceAll('-default:', ':');
 
-const css = cssHex + cssRgb;
+  cssRgb = cssRgb.replaceAll(regex, '$1');
 
-let { code } = transform({
-  filename: '',
-  code: Buffer.from(css),
-  //minify: true,
+  const tokens = tokenize(`./tokens/${brandToName(brandMode).replace('-', '.')}`);
+
+  const css = cssHex + cssRgb + tokens;
+
+  let { code } = transform({
+    filename: '',
+    code: Buffer.from(css),
+    //minify: true,
+  });
+  const output = code.toString();
+
+  console.log(`Outputting ${brandMode}...`);
+  // Outputting to a temp directory for now
+  fs.outputFileSync(`./dist-new/${brandToName(brandMode)}.css`, output, 'utf8');
 });
-const output = code.toString();
-
-fs.outputFileSync('./dist-new/FINN/final.css', output, 'utf8');
--- utils.js
+++ utils.js
@@ -3,10 +3,40 @@ import { Readable } from 'node:stream';
 import { finished } from 'node:stream/promises';
 
 import fs from 'fs-extra';
+import unzipper from 'unzipper';
 
-export const downloadFile = async (url, fileName) => {
-  const res = await fetch(url);
-  const destination = path.resolve('./tmp', fileName);
+const webReleasesUrl = 'https://github.com/warp-ds/tokens/releases/download/latest/web.zip';
+const webReleasesFileName = 'web.zip';
+const tempDir = 'tmp/';
+const outputDir = 'output/web';
+
+export const init = () => {
+  console.log('Initializing setup...');
+  fs.removeSync(tempDir);
+  fs.removeSync(outputDir);
+  fs.ensureDirSync(tempDir);
+  fs.ensureDirSync(outputDir);
+};
+
+export const downloadReleaseFile = async () => {
+  console.log('Downloading assets from tokens repo: web.zip...');
+  const res = await fetch(webReleasesUrl);
+  const destination = path.resolve(tempDir, webReleasesFileName);
   const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
   await finished(Readable.fromWeb(res.body).pipe(fileStream));
+  console.log('Extracting asset...');
+  await unzipper.Open.file(tempDir + webReleasesFileName).then((d) => d.extract({ path: outputDir, concurrency: 5 }));
+};
+
+export const brandToName = (brand) => {
+  switch (brand) {
+    case 'FINN light':
+      return 'finn-no';
+    case 'Blocket light':
+      return 'blocket-se';
+    case 'DBA light':
+      return 'dba-dk';
+    case 'TORI light':
+      return 'tori-fi';
+  }
 };
