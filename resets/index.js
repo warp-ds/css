34fa1d9bd3cef5240e470625cb3ace6bccc2bab7
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2024-07-03 13:36:51 +0200

    fix: update @warp-ds/uno peerDep and devDep to version 2.x

--- resets/index.js
+++ resets/index.js
@@ -3,13 +3,25 @@ import path from 'node:path';
 
 import drnm from 'drnm';
 
-import { minify } from '../css-minify.js';
+import * as lightning from 'lightningcss';
 
 import { resets } from './resets.js';
 
 const __dirname = drnm(import.meta.url);
 const outPath = path.join(__dirname, '../dist');
 fs.mkdirSync(outPath, { recursive: true });
+
+const minify = (css) => {
+  const { code } = lightning.transform({
+    code: Buffer.from(css),
+    minify: true,
+    targets: {
+      safari: 13 << 16,
+    },
+  });
+
+  return code.toString();
+};
 
 function process(tld) {
   const css = minify(resets);
