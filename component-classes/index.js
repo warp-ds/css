90ecde69caeb3fdf5665c0c02c71142f0763aaf1
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2024-07-22 10:52:13 +0200

    fix(buttongroup): clean up classes

--- component-classes/index.js
+++ component-classes/index.js
@@ -323,14 +323,16 @@ export const buttonGroup = {
 
 export const buttonGroupItem = {
   wrapper:
-    'relative s-text s-bg hover:bg-[--w-color-buttongroup-utility-background-hover] active:s-text active:bg-[--w-color-buttongroup-utility-background-selected]',
-  outlined: 'border hover:z-30 border-[--w-color-buttongroup-utility-border] active:border-[--w-color-buttongroup-utility-border-selected]',
+    'relative s-text',
+  outlined: 'border hover:z-30',
   outlinedVertical: '-mb-1 last:mb-0 first:rounded-lt-4 first:rounded-rt-4 last:rounded-lb-4 last:rounded-rb-4',
   outlinedHorizontal: '-mr-1 last:mr-0 first:rounded-lt-4 first:rounded-lb-4 last:rounded-rt-4 last:rounded-rb-4',
   outlinedVerticalResets: 'px-1 pt-1 last:pb-1 -mb-1 last:mb-0',
   outlinedHorizontalResets: 'py-1 pl-1 last:pr-1 -mr-1 last:mr-0',
+  outlinedUnselected: 'border-[--w-color-buttongroup-utility-border] active:border-[--w-color-buttongroup-utility-border-selected]',
   outlinedSelected: 'border-[--w-color-buttongroup-utility-border-selected]',
-  selected: 'z-30 s-text! bg-[--w-color-buttongroup-utility-background-selected]!',
+  unSelected: 's-bg hover:bg-[--w-color-buttongroup-utility-background-hover] active:s-text active:bg-[--w-color-buttongroup-utility-background-selected]',
+  selected: 'z-30 bg-[--w-color-buttongroup-utility-background-selected]',
 };
 
 export const modal = {
