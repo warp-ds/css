0cc3a0efcf6151cdd29585ae0148c53d2aaa7b75
felicia-haggqvist <144792260+felicia-haggqvist@users.noreply.github.com>
2024-08-01 14:24:27 +0200

    fix(clickable): clean up classes (#231)
    refactor clickable.label to not have important to override padding-bottom

--- component-classes/index.js
+++ component-classes/index.js
@@ -517,7 +517,7 @@ export const deadToggle = {
 
 export const clickable = {
   toggle: 'absolute inset-0 h-full w-full appearance-none cursor-pointer focusable focusable-inset',
-  label: `px-12 ${label.label} py-8! cursor-pointer focusable focusable-inset`,
+  label: 'antialiased block relative text-s font-bold s-text px-12 py-8 cursor-pointer focusable focusable-inset',
   buttonOrLink: 'bg-transparent focusable',
   buttonOrLinkStretch: 'inset-0 absolute',
 };
